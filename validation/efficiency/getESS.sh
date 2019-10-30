0f58d928bdf95b5456fa66a3c222140be5ddc310
ryanzhang <rzha419@aucklanduni.ac.nz>
2019-10-30 17:22:08 +1300

    add results for efficiency compare

--- validation/efficiency/getESS.sh
+++ validation/efficiency/getESS.sh
@@ -10,13 +10,17 @@
 #done
 
 
-for length in {Short,Medium}
-do
-	for model in {Cons,Category}
-	do
-	/Applications/BEAST2.6.0/bin/loganalyser -oneline /Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/simulated/20taxa/${length}${model}/logs/*.log >/Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/simulated/20taxa/ess/ESS_${length}${model}20taxa.txt
-	done
-done
+#for length in {Short,Medium}
+#do
+	#for model in {Cons,Category}
+	#do
+	#/Applications/BEAST2.6.0/bin/loganalyser -oneline /Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/simulated/20taxa/${length}${model}/logs/*.log >/Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/simulated/20taxa/ess/ESS_${length}${model}20taxa.txt
+	#done
+#done
 
+for model in {Cons,Category}
+do
+/Applications/BEAST2.6.0/bin/loganalyser -oneline /Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/others/primates${model}/logs/*.log >/Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/others/ess/ESS_primates${model}.txt
+done
 
 
