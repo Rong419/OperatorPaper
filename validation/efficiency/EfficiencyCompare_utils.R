9ba013686db9bf13538f7e593910495ff7de13db
ryanzhang <rzha419@aucklanduni.ac.nz>
2019-11-06 10:13:50 +1300

    add results

--- validation/efficiency/EfficiencyCompare_utils.R
+++ validation/efficiency/EfficiencyCompare_utils.R
@@ -95,3 +95,23 @@ get.anolis.efficiency <- function(txt.df, time.df) {
   data = data.frame(cbind(posterior,likelihood,prior,birth.rate,death.rate,tree.height,tree.length,ucld.stdev,rate.mean,rate.variance,rate.coeff,kappa,frequency1,frequency2,frequency3,frequency4))
   return (data)
 }
+
+get.primates.efficiency <- function(txt.df, time.df) {
+  posterior = txt.df$posterior.ESS / time.df
+  likelihood = txt.df$likelihood.ESS / time.df
+  prior = txt.df$prior.ESS / time.df
+  tree.height = txt.df$Tree.height.ESS / time.df
+  tree.length = txt.df$Tree.treeLength.ESS / time.df
+  ucld.stdev = txt.df$ucldStdev.ESS / time.df
+  birth.rate = txt.df$birthRate.ESS / time.df
+  rate.mean = txt.df$rate.mean.ESS / time.df
+  rate.variance = txt.df$rate.variance.ESS / time.df
+  rate.coeff = txt.df$rate.coefficientOfVariation.ESS / time.df
+  kappa = txt.df$kappa.ESS / time.df
+  frequency1 = txt.df$freqParameter.1.ESS / time.df
+  frequency2 = txt.df$freqParameter.2.ESS / time.df
+  frequency3 = txt.df$freqParameter.3.ESS / time.df
+  frequency4 = txt.df$freqParameter.4.ESS / time.df
+  data = data.frame(cbind(posterior,likelihood,prior,tree.height,tree.length,birth.rate,ucld.stdev,rate.mean,rate.variance,rate.coeff,kappa,frequency1,frequency2,frequency3,frequency4))
+  return (data)
+}
--- validation/efficiency/getESS.sh
+++ validation/efficiency/getESS.sh
@@ -2,25 +2,25 @@
 
 for data in {Shankarappa,RSV2}
 do
-model=Cons
-	#for model in {Cons,Category}
-	#do
+	for model in {Cons,Category}
+	do
 	/Applications/BEAST2.6.0/bin/loganalyser -oneline /Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/others/${data}${model}/logs/*.log >/Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/others/ess/ESS_${data}${model}.txt
-	#done	
+	done	
 done
 
 
-#for length in {Short,Medium}
-#do
-	#for model in {Cons,Category}
-	#do
-	#/Applications/BEAST2.6.0/bin/loganalyser -oneline /Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/simulated/20taxa/${length}${model}/logs/*.log >/Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/simulated/20taxa/ess/ESS_${length}${model}20taxa.txt
-	#done
-#done
+for length in {Short,Medium}
+do
+	for model in {Cons,Category}
+	do
+	/Applications/BEAST2.6.0/bin/loganalyser -oneline /Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/simulated/20taxa/${length}${model}/logs/*.log >/Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/simulated/20taxa/ess/ESS_${length}${model}20taxa.txt
+	done
+done
 
-#for model in {Cons,Category}
-#do
-#/Applications/BEAST2.6.0/bin/loganalyser -oneline /Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/others/primates${model}/logs/*.log >/Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/others/ess/ESS_primates${model}.txt
-#done
+
+for model in {Cons,Category}
+do
+/Applications/BEAST2.6.0/bin/loganalyser -oneline /Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/others/primates${model}/logs/*.log >/Users/ryanzhang/Documents/UOALearning/OperatorPaper/validation/efficiency/others/ess/ESS_primates${model}.txt
+done
 
 
